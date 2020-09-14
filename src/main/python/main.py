882143713303ee793c04d8d852f55157ae03116a
phild <zhe.dong@icahn.mssm.edu>
2020-09-14 15:43:26 -0400

    add slider and class dropdown

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -16,9 +16,10 @@ from PyQt5.QtWidgets import (
     QHBoxLayout,
     QComboBox,
     QLabel,
+    QSlider,
+    QPushButton,
 )
 from vispy import scene
-from vispy.app import Timer
 from vispy.color import ColorArray
 from vispy.visuals.filters import Alpha
 from bokeh.palettes import Category20_20
@@ -37,8 +38,15 @@ class MainWindow(QMainWindow):
         QMainWindow.__init__(self)
         self.resize(700, 500)
         self.setWindowTitle("Isomap Vispy Viewer")
+        # playing timer
+        self.isPlaying = False
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.timer_play)
+        self.timer.start(int(1000 / CONFIG["fps"]))
         # load data
-        df = pd.read_feather(os.path.join(FPATH, CONFIG["df_path"]))
+        df = pd.read_feather(os.path.join(FPATH, CONFIG["df_path"])).astype(
+            {CONFIG["col_names"]["class"]: str}
+        )
         # meta widgets
         layout_meta = QVBoxLayout()
         self.metas = {d: df[d].unique() for d in CONFIG["meta_dims"]}
@@ -55,23 +63,66 @@ class MainWindow(QMainWindow):
             layout_meta.addLayout(widget)
         self.df = df.set_index(list(self.meta.keys())).sort_index()
         # vispy
-        data = self.df.loc[tuple(self.meta.values())].copy()
-        self.canvas = isoVis(data=data)
+        self.data = (
+            self.df.loc[tuple(self.meta.values())]
+            .copy()
+            .reset_index(drop=True)
+            .sort_values(CONFIG["col_names"]["frame"])
+            .reset_index(drop=True)
+        )
+        self.canvas = isoVis(data=self.data)
         self.canvas.create_native()
         self.canvas.native.setParent(self)
+        # player
+        self.slider = QSlider(QtCore.Qt.Horizontal)
+        self.slider.setMinimum(0)
+        self.slider.setMaximum(self.data.index.max())
+        self.slider.setSingleStep(1)
+        self.slider.setValue(0)
+        self.slider.sliderReleased.connect(self.snap_frame)
+        self.slider.valueChanged.connect(self.canvas.fm_change)
+        self.ply_button = QPushButton("Play")
+        self.ply_button.setCheckable(True)
+        self.ply_button.setChecked(False)
+        self.ply_button.pressed.connect(self.play)
+        layout_player = QHBoxLayout()
+        layout_player.addWidget(self.ply_button)
+        layout_player.addWidget(self.slider)
+        # class
+        widget_class = QComboBox()
+        widget_class.addItems(
+            ["all"] + self.data[CONFIG["col_names"]["class"]].unique().tolist()
+        )
+        widget_class.setCurrentIndex(0)
+        self.subfm = self.data[CONFIG["col_names"]["frame"]]
+        widget_class.currentTextChanged.connect(self.class_change)
+        lab_class = QLabel("class")
+        layout_class = QHBoxLayout()
+        layout_class.addWidget(lab_class)
+        layout_class.addWidget(widget_class)
         # master layout
         self.layout_master = QGridLayout()
         self.layout_master.addWidget(self.canvas.native, 0, 0)
         self.layout_master.addLayout(layout_meta, 0, 1)
-        self.layout_master.setColumnStretch(0, 5)
+        self.layout_master.addLayout(layout_player, 1, 0)
+        self.layout_master.addLayout(layout_class, 1, 1)
+        self.layout_master.setColumnStretch(0, 0)
         self.layout_master.setColumnStretch(1, 1)
+        self.layout_master.setRowStretch(0, 0)
+        self.layout_master.setRowStretch(1, 1)
         widget_master = QWidget()
         widget_master.setLayout(self.layout_master)
         self.setCentralWidget(widget_master)
 
     def meta_change(self, lab, dim):
         self.meta[dim] = lab
-        data = self.df.loc[tuple(self.meta.values())].copy()
+        data = (
+            self.df.loc[tuple(self.meta.values())]
+            .copy()
+            .reset_index(drop=True)
+            .sort_values(CONFIG["col_names"]["frame"])
+            .reset_index(drop=True)
+        )
         self.layout_master.removeWidget(self.canvas.native)
         self.canvas.native.close()
         self.canvas = isoVis(data=data)
@@ -79,20 +130,43 @@ class MainWindow(QMainWindow):
         self.canvas.native.setParent(self)
         self.layout_master.addWidget(self.canvas.native, 0, 0)
 
+    def play(self):
+        if self.ply_button.isChecked():
+            self.isPlaying = False
+            self.ply_button.setText("Play")
+        else:
+            self.isPlaying = True
+            self.ply_button.setText("Pause")
+
+    def timer_play(self):
+        if self.isPlaying:
+            curfm = self.slider.value()
+            try:
+                nxtfm = self.subfm[np.searchsorted(self.subfm, curfm) + 1]
+            except IndexError:
+                nxtfm = self.subfm[np.searchsorted(self.subfm, curfm)]
+            self.slider.setValue(nxtfm)
+
+    def snap_frame(self):
+        curfm = self.slider.value()
+        nxtfm = self.subfm[np.searchsorted(self.subfm, curfm)]
+        self.slider.setValue(nxtfm)
+
+    def class_change(self, class_lab):
+        if class_lab == "all":
+            self.subfm = self.data.index
+        else:
+            self.subfm = self.data[
+                self.data[CONFIG["col_names"]["class"]] == class_lab
+            ].index
+        self.snap_frame()
+
 
 class isoVis(scene.SceneCanvas):
     def __init__(self, data=None, *args, **kwargs) -> None:
         # init
         scene.SceneCanvas.__init__(self, *args, keys="interactive", **kwargs)
         self.unfreeze()
-        self.timer = Timer(
-            interval=1 / 10,
-            connect=self.on_timer,
-            app=self.app,
-            start=True,
-            iterations=999,
-        )
-        self.counter = 0
         self.grid = self.central_widget.add_grid(margin=10)
         # color data
         col_cls = CONFIG["col_names"]["class"]
@@ -131,21 +205,20 @@ class isoVis(scene.SceneCanvas):
         im_title.height_max = 30
         self.grid.add_widget(im_title, row=0, col=1)
         self.im_view = self.grid.add_view(row=1, col=1, border_color="white")
-        self.im_data = np.random.random(size=(1000, 100, 200))
+        self.im_data = np.random.random(size=(len(self.sct_data), 100, 100))
         self.im = scene.Image(parent=self.im_view.scene, data=self.im_data[0, :, :])
         self.im_view.camera = "panzoom"
         self.im_view.camera.rect = (0, 0, self.im_data.shape[1], self.im_data.shape[2])
 
-    def on_timer(self, event=None):
-        self.counter += 1
+    def fm_change(self, ifm):
         self.cur_mks.set_data(
             pos=np.expand_dims(
-                self.sct_data.iloc[self.counter, :][["comp0", "comp1", "comp2"]].values,
+                self.sct_data.loc[ifm, ["comp0", "comp1", "comp2"]].values,
                 axis=0,
             ),
-            face_color=self.sct_data.iloc[self.counter, :]["cstrong"],
+            face_color=self.sct_data.loc[ifm, "cstrong"],
         )
-        self.im.set_data(self.im_data[self.counter, :, :])
+        self.im.set_data(self.im_data[ifm, :, :])
         self.update()
 
 
